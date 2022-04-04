34e637566ef826abee8aa13906d7afb462e843df
Hydil Aicard Sokeing <shydilaicard@gmail.com>
2022-04-04 16:33:48 +0100

    Update the database config file

--- src/main/java/cm/elshaseries/portfolio/config/DatabaseConfig.java
+++ src/main/java/cm/elshaseries/portfolio/config/DatabaseConfig.java
@@ -12,7 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DatabaseConfig {
 
-    @Value("${spring.datasource.url}")
+    @Value("${HEROKU_POSTGRESQL_NAVY_URL}")
     private String dbUrl;
   
     @Bean
